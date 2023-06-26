374dbfb5b5ce7e0c3222fd753942daa1a4ce3f3b
Christian2650 <128227321+Christian2650@users.noreply.github.com>
2023-06-26 17:32:12 +0200

    Add files via upload

---
+++ 1_delegowanie_zadan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int czas = 332;
+
+        string formatowanyCzas = FormatujCzas(czas);
+        Console.WriteLine(formatowanyCzas);
+    }
+
+    static string FormatujCzas(int czas)
+    {
+        int godziny = czas / 3600;
+        int minuty = (czas % 3600) / 60;
+        int sekundy = czas % 60;
+
+        string sGodziny = godziny.ToString("D2");
+        string sMinuty = minuty.ToString("D2");
+        string sSekundy = sekundy.ToString("D2");
+
+        string formatowanyCzas = $"{sGodziny}:{sMinuty}:{sSekundy}";
+
+        return formatowanyCzas;
+    }
+}
---
+++ 2_delegowanie_zadan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x1 = 1.5;
+        double y1 = 2.3;
+        double x2 = 4.8;
+        double y2 = 6.1;
+
+        double dlugoscOdcinka = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+        Console.WriteLine("Długość odcinka: " + dlugoscOdcinka);
+    }
+
+    static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+    {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+
+        double dlugosc = Math.Sqrt(dx * dx + dy * dy);
+
+        return dlugosc;
+    }
+}
